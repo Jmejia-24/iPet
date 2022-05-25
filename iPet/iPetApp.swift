6052b57b41ea72ce68ec6474c093d49623b6f167
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-05-25 00:03:55 -0600

    Initial Commit

---
+++ iPet/iPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPetApp.swift
+//  iPet
+//
+//  Created by Byron on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct iPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
